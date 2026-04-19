4d1c969f6642ce2080dd629108144e4e6cf7c444
Marcos Lima <mrsk1414@gmail.com>
2026-04-19 05:51:05 -0300

    feat: add news fetching functionality and layout updates in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import Title from "../components/Title";
 import Update from "../components/Update";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  const [news, setNews] = useState([]);
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+  useEffect(() => {
+    async function loadNews() {
+      try {
+        const response = await axios.get(`${API_URL}/news`);
+        setNews(response.data);
+      } catch (error) {
+        console.error("Erro ao carregar notícias:", error);
+      }
+    }
+    loadNews();
+  }, []);
+
   return (
     <Container>
       <Header>
@@ -13,20 +30,43 @@ export default function Home() {
       <Body>
         <LeftSide>
           <MainBox>
-            <ToNews to="/1">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              consectetur adipiscing.
-            </ToNews>
-            <p>CNN Brasil</p>
+            <img src={news?.[0]?.imageUrl} />
+            <div className="overlay" />
+            <ToNews to="/1">{news?.[0]?.title}</ToNews>
+            <p>{news?.[0]?.portal}</p>
           </MainBox>
-          <FirstCollummn>
-            <SmallBox></SmallBox>
-            <SmallBox></SmallBox>
-          </FirstCollummn>
-          <SecondCollumn>
-            <SmallBox></SmallBox>
-            <SmallBox></SmallBox>
-          </SecondCollumn>
+          <FirstRow>
+            <FrameBox>
+              <SmallBox>
+                <img src={news?.[1]?.imageUrl} />
+              </SmallBox>
+              <SmallTitle to="/1">{news?.[1]?.title}</SmallTitle>
+              <p>{news?.[0]?.portal}</p>
+            </FrameBox>
+            <FrameBox>
+              <SmallBox>
+                <img src={news?.[2]?.imageUrl} />
+              </SmallBox>
+              <SmallTitle to="/1">{news?.[2]?.title}</SmallTitle>
+              <p>{news?.[0]?.portal}</p>
+            </FrameBox>
+          </FirstRow>
+          <SecondRow>
+            <FrameBox>
+              <SmallBox>
+                <img src={news?.[3]?.imageUrl} />
+              </SmallBox>
+              <SmallTitle to="/1">{news?.[3]?.title}</SmallTitle>
+              <p>{news?.[0]?.portal}</p>
+            </FrameBox>
+            <FrameBox>
+              <SmallBox>
+                <img src={news?.[4]?.imageUrl} />
+              </SmallBox>
+              <SmallTitle to="/1">{news?.[3]?.title}</SmallTitle>
+              <p>{news?.[0]?.portal}</p>
+            </FrameBox>
+          </SecondRow>
         </LeftSide>
         <RightSide>
           <SideBox></SideBox>
@@ -64,56 +104,163 @@ const Body = styled.div`
 `;
 
 const MainBox = styled.div`
+  position: relative;
   height: 307px;
-  width: 482px;
+  width: 520px;
   display: flex;
+  font-size: 20px;
   flex-direction: column;
   justify-content: end;
   align-items: start;
   margin-bottom: 18px;
-  border-radius: 8px;
+  border-radius: 12px;
   background-color: #282828;
+  overflow: hidden;
 
   p {
     font-family: "Chivo Mono", monospace;
+    height: 20px;
+    width: fit-content;
+    background-color: #bedd0a;
     font-size: 12px;
-    color: #656363;
+    color: #353434;
     margin-bottom: 24px;
-    margin-left: 15px;
+    margin-left: 25px;
+    position: relative;
+    z-index: 2;
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    border-radius: 3px;
+  }
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    z-index: 0;
+  }
+
+  .overlay {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 40%;
+    width: 100%;
+    background: linear-gradient(
+      to top,
+      rgba(0, 0, 0, 0.95) 0%,
+      rgba(0, 0, 0, 0.8) 20%,
+      rgba(0, 0, 0, 0.4) 50%,
+      rgba(0, 0, 0, 0) 100%
+    );
+    backdrop-filter: blur(1px);
+    -webkit-backdrop-filter: blur(1px);
+
+    z-index: 1;
+    pointer-events: none;
+    border-radius: 0 0 12px 12px;
   }
 `;
 
 const ToNews = styled(Link)`
   font-family: "Merriweather", serif;
   font-style: italic;
-  font-size: 20px;
   color: white;
-  padding: 0px 15px 10px 15px;
+  padding: 0px 15px 10px 25px;
   text-decoration: none;
+  position: relative;
+  z-index: 2;
 `;
 
 const LeftSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
   margin-right: 18px;
 `;
 
-const FirstCollummn = styled.div`
+const FirstRow = styled.div`
+  width: 520px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 18px;
+`;
+
+const SecondRow = styled.div`
+  width: 520px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
 `;
 
-const SecondCollumn = styled.div`
+const FrameBox = styled.div`
+  height: 270px;
+  width: 232px;
+  border-radius: 12px;
+  background-color: #3c3b3b;
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: start;
+  padding: 10px;
+  position: relative;
+
+  p {
+    font-family: "Chivo Mono", monospace;
+    height: 20px;
+    width: fit-content;
+    background-color: #bedd0a;
+    font-size: 12px;
+    color: #353434;
+    margin-bottom: 24px;
+    margin-left: 5px;
+    position: absolute;
+    z-index: 2;
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    border-radius: 3px;
+    bottom: 0;
+    left: 10;
+  }
 `;
 
 const SmallBox = styled.div`
+  position: relative;
+
   height: 147px;
+  max-height: 120px;
   width: 232px;
   margin-bottom: 18px;
-  border-radius: 8px;
+  border-radius: 12px;
   background-color: #282828;
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-height: 120px;
+    height: 100%;
+    width: 100%;
+    border-radius: 12px;
+    object-fit: cover;
+    z-index: 0;
+  }
+`;
+
+const SmallTitle = styled(Link)`
+  font-family: "Merriweather", serif;
+  font-style: italic;
+  color: white;
+  padding: 0px 15px 10px 5px;
+  text-decoration: none;
+  position: relative;
+  z-index: 2;
 `;
 
 const RightSide = styled.div`
@@ -125,6 +272,6 @@ const SideBox = styled.div`
   height: 200px;
   width: 321px;
   margin-bottom: 18px;
-  border-radius: 8px;
+  border-radius: 12px;
   background-color: #282828;
 `;
